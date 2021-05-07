05f327c7202b3c394ec4a499bc5d45cca7405207
Ismael <35577277+Equipo45@users.noreply.github.com>
2021-05-07 22:19:11 +0200

    Create tipos_datos.py

---
+++ tipos_datos.py
@@ -0,0 +1,21 @@
+proposicion_1=True
+proposicion_2=False
+print(type(proposicion_2))
+if proposicion_2:
+	print('Verdadero')
+
+
+entero=44
+print(entero)
+print(entero/23)
+
+lista=[1,2,4]
+diccionario={'a':'b'}
+tupla={1,2,3,4,6}
+
+print(type(lista),type(diccionario),type(tupla))
+
+cadena="hola"
+lista=['h','o','l','a']
+print(cadena[0])
+print(cadena[0])
